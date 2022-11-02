88fe211383c6aec16143d9d5982a32d4292cee7f
Melissa Fields <melissamfields12@gmail.com>
2022-11-02 12:05:59 -0700

    commit tree f439b88bf73e8c363193046c13599272d14bbccb

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  //reverse the input string
+  const reversedWord = reverseString(word);
+  //If the reversed string is the same as input
+  return word === reversedWord;
+  }
+function reverseString(word) {
+  return word.split("").reverse().join("")
 }
-
 /* 
   Add your pseudocode here
+  Reverse the input string
+  
+  If the reversed string is the same as the input, return true
+  else 
+  return false
 */
 
 /*
@@ -20,6 +31,26 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+  console.log("Expecting: ih");
+  console.log("=>", reverseString("hi"));
+  
+  console.log("");
+  
+  console.log("Expecting: tobor");
+  console.log("=>", reverseString("robot"));
+  
+  console.log("");
+  
+  console.log("Expecting: mom");
+  console.log("=>", reverseString("mom"));
+
+
 }
 
 module.exports = isPalindrome;
